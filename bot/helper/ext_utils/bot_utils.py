bf5a1668a5bdf0cbf60acc0b69e281694e89b756
Aaron-p-saji <65056958+Aaron-p-saji@users.noreply.github.com>
2022-07-15 14:31:45 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -178,7 +178,7 @@ def get_readable_message():
                 elif 'MB/s' in spd:
                     upspeed_bytes += float(spd.split('M')[0]) * 1048576
         bmsg += f"\n<b>DL:</b> {get_readable_file_size(dlspeed_bytes)}/s | <b>UL:</b> {get_readable_file_size(upspeed_bytes)}/s"
-            buttons = ButtonMaker()
+         buttons = ButtonMaker()
             buttons.buildbutton("👑 OWNER 👑", "https://t.me/RubyMathews_Bot")
             sts_button = InlineKeyboardMarkup(buttons.build_menu(2))
         if STATUS_LIMIT is not None and tasks > STATUS_LIMIT:
