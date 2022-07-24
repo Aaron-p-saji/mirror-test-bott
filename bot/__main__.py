e61a34cfe665df15c71057a4496d9e5a518aa590
Aaron-p-saji <65056958+Aaron-p-saji@users.noreply.github.com>
2022-07-24 21:59:15 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -270,7 +270,7 @@ def main():
         osremove(".restartmsg")
 
     start_handler = CommandHandler(BotCommands.StartCommand, start, run_async=True)
-    info_handler = CommandHandler(BotCommands.InfoCommand, start, run_async=True)
+    info_handler = CommandHandler(BotCommands.InfoCommand, info, run_async=True)
     ping_handler = CommandHandler(BotCommands.PingCommand, ping,
                                   filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
     restart_handler = CommandHandler(BotCommands.RestartCommand, restart,
