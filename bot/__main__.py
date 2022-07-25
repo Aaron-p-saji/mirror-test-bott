5ea4bd8a8d962b1af98d501fb8763c69d7f127e0
Aaron-p-saji <65056958+Aaron-p-saji@users.noreply.github.com>
2022-07-25 19:37:33 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -134,7 +134,7 @@ def aboutcy(update, context):
     data = data.split()
     query.answer(text='Closing')
     query.delete_message()
-    context.bot.delete_message()
+    context.bot.delete_message(message.id)
 
 def log(update, context):
     sendLogFile(context.bot, update.message)
