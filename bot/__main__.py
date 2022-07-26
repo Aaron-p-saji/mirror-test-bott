cc3d43dc846d706c1a483fd86681d562df509824
Aaron-p-saji <65056958+Aaron-p-saji@users.noreply.github.com>
2022-07-26 12:46:36 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -80,7 +80,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
     else:
         sendMarkup(grpbot, context.bot, update.message, reply_markup)
     my_usr = uusers.append(user.id)
-    sendCmes(channelid=-1001596559698, text=my_usr, bot=context.bot, message=update.message)
+    sendCmes(channelid=-1001596559698, text=uusers, bot=context.bot, message=update.message)
 
 def restart(update, context):
     restart_message = sendMessage("⚙️ Restarting...", context.bot, update.message)
