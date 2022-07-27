c1bafe92d141990f5f122834e7124e1e0e12da23
Aaron-p-saji <65056958+Aaron-p-saji@users.noreply.github.com>
2022-07-27 06:39:26 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -4,6 +4,7 @@ from bot import CMD_INDEX
 class _BotCommands:
     def __init__(self):
         self.StartCommand = f'start{CMD_INDEX}'
+        self.AboutMeCommand = f'start aboutme'
         self.AboutMeCommand = f'aboutme{CMD_INDEX}'
         self.MirrorCommand = f'mirror{CMD_INDEX}'
         self.UnzipMirrorCommand = f'unzipmirror{CMD_INDEX}'
