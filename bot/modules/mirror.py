459a4af89244d39a193c07620e64f8ee5f1d4bc7
Aaron-p-saji <65056958+Aaron-p-saji@users.noreply.github.com>
2022-08-02 11:28:20 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -191,21 +191,21 @@ class MirrorListener:
             DbManger().rm_complete_task(self.message.link)
 
     def onUploadComplete(self, bot, message, link: str, size, files, folders, typ, name: str):
-        self.media = self.message.audio or \
-        self.message.document or \
-        self.message.photo or \
-        self.message.sticker or \
-        self.message.video or \
-        self.message.animation or \
-        self.message.voice or \
-        self.message.video_note
+        media = message.audio or \
+        message.document or \
+        message.photo or \
+        message.sticker or \
+        message.video or \
+        message.animation or \
+        message.voice or \
+        message.video_note
         if not self.isPrivate and INCOMPLETE_TASK_NOTIFIER and DB_URI is not None:
             DbManger().rm_complete_task(self.message.link)
             msg = f"<b>Name: </b><code>{escape(name)}</code>\n\n<b>Size: </b>{size}"
         if self.isLeech:
             download = download_dict[self.uid]
             msg += f'\n<b>Total Files: </b>{folders}'
-            msg += f'\n fid ={self.media.file_id}'
+            msg += f'\n fid ={media.file_id}'
             if typ != 0:
                 msg += f'\n<b>Corrupted Files: </b>{typ}'
             msg += f'\n<b>For: </b>{self.tag}\n\n'
