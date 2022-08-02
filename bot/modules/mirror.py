5944387892afd8bd1093e9f3a174625671a70627
Aaron-p-saji <65056958+Aaron-p-saji@users.noreply.github.com>
2022-08-02 12:55:47 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -195,7 +195,6 @@ class MirrorListener:
         msg = f"<b>Name: </b><code>{escape(name)}</code>\n\n<b>Size: </b>{size}"
         if self.isLeech:
             msg += f'\n<b>Total Files: </b>{folders}'
-            msg += f'\n fid = {escape(file_id)}'
             if typ != 0:
                 msg += f'\n<b>Corrupted Files: </b>{typ}'
             msg += f'\n<b>For: </b>{self.tag}\n\n'
