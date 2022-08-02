3173f8af5bd9b39c447b26b9334a8a493ab9c2cc
Aaron-p-saji <65056958+Aaron-p-saji@users.noreply.github.com>
2022-08-02 11:33:19 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -190,15 +190,7 @@ class MirrorListener:
         if not self.isPrivate and INCOMPLETE_TASK_NOTIFIER and DB_URI is not None:
             DbManger().rm_complete_task(self.message.link)
 
-    def onUploadComplete(self, bot, message, link: str, size, files, folders, typ, name: str):
-        media = message.audio or \
-        message.document or \
-        message.photo or \
-        message.sticker or \
-        message.video or \
-        message.animation or \
-        message.voice or \
-        message.video_note
+    def onUploadComplete(self, link: str, size, files, folders, typ, name: str):
         if not self.isPrivate and INCOMPLETE_TASK_NOTIFIER and DB_URI is not None:
             DbManger().rm_complete_task(self.message.link)
             msg = f"<b>Name: </b><code>{escape(name)}</code>\n\n<b>Size: </b>{size}"
